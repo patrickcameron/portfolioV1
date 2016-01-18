4017b6aac53d1570cfcb135a5f30ede0743b8c93
Patrick Cameron <patcameron@gmail.com>
2016-01-17 21:30:53 -0500

    Initial commit

---
+++ javascript/scripts.js
@@ -0,0 +1,23 @@
+$(function() {
+
+	$(window).scroll(function () {
+	    if ($(this).scrollTop() > 50) {
+	        $('body').addClass('colorChange')
+	        $('header').addClass('displayNone')
+	        $('nav').removeClass('navBackgroundStart')
+	        $('nav ul').addClass('addBlackBackground')
+	    } 
+	    if ($(this).scrollTop() < 50) {
+	        $('body').removeClass('colorChange')
+	        $('header').removeClass('displayNone')
+	        $('nav').addClass('navBackgroundStart')
+	        $('nav ul').removeClass('addBlackBackground')
+	    } 
+	});
+
+	$('a.smoothScroll').smoothScroll({
+		speed: 400
+	});
+
+});
+
